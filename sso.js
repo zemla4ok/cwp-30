0e32592f1a2bdbd6478f01ceb8738c296017a783
zemla4ok <dima-vital@live.com>
2018-04-03 19:59:12 +0300

    input code from

--- sso.js
+++ sso.js
@@ -30,6 +30,45 @@ app.get('/login', (req, res) => {
     }
 })
 
+app.post('/login', async (req, res) => {
+    const user = await db.user.findOne({
+        where: {
+            email: req.body.email
+        },
+        raw: true
+    })
+
+    if(!user || !bcrypt.compareSync(req.body.pass, user.password)){
+        res.json({code: 400, message: 'Invalid data'});
+    }
+    else{
+        codes = JSON.parse(user.codes);
+        const num = Math.floor(Math.random() * 4) + 1;
+
+        code = codes[num];
+
+        const token = jwt.sign({
+                'id': user.id
+            },
+            'secret',
+            {
+                expiresIn: 2*60
+            });
+
+        res.redirect(`/codes?source=/protected_resource&callback=localhost:7000/token&token=${token}&code=${num}`);
+    }
+})
+
+app.get('/codes', (req, res) => {
+    res.cookie("temptoken", req.query.token);
+
+    res.sendFile(__dirname + '/codes.html');
+})
+
+app.post('/codes', (req, res) => {
+    
+})
+
 app.listen(port, async () => {
     console.log('sso is running on port ' + port);
 })
